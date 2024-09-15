bcb8522ce20a48f97b97e99304e0e2e3f5f56323
saraklamann <saracarolineklamannjucoski@gmail.com>
2024-09-15 13:50:20 -0300

    feat(API): Método GET ALL funcionando

--- backend/src/main/java/com/kingspan/backend/controllers/SystemController.java
+++ backend/src/main/java/com/kingspan/backend/controllers/SystemController.java
@@ -1,11 +1,12 @@
 package com.kingspan.backend.controllers;
 
+import java.util.List;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +26,10 @@ public class SystemController {
         var systemModel = new SystemModel();
         BeanUtils.copyProperties(systemRecordDto, systemModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(systemRepository.save(systemModel));
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<SystemModel>> getAllSystems(){
+        return ResponseEntity.status(HttpStatus.OK).body(systemRepository.findAll());
     }
 }
