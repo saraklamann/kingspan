60cb636d67c53eec659e6b9cc8434e1736029e1d
saraklamann <saracarolineklamannjucoski@gmail.com>
2024-09-15 13:15:23 -0300

    feat(API): Método POST funcionando

--- backend/src/main/java/com/kingspan/backend/controllers/SystemController.java
+++ backend/src/main/java/com/kingspan/backend/controllers/SystemController.java
@@ -1,18 +1,29 @@
 package com.kingspan.backend.controllers;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.kingspan.backend.dtos.SystemRecordDto;
 import com.kingspan.backend.models.SystemModel;
 import com.kingspan.backend.repositories.SystemRepository;
-
+import jakarta.validation.Valid;
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class SystemController {
 
     @Autowired
     SystemRepository systemRepository;
+
+    @PostMapping(value = "")
+    public ResponseEntity <SystemModel> saveSystem(@RequestBody @Valid SystemRecordDto systemRecordDto){
+        var systemModel = new SystemModel();
+        BeanUtils.copyProperties(systemRecordDto, systemModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(systemRepository.save(systemModel));
+    }
 }
