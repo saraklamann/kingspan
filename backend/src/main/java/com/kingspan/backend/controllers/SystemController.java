77bd6a7dd4ca5e10959567576ccd2868904c1397
saraklamann <saracarolineklamannjucoski@gmail.com>
2024-09-15 14:01:06 -0300

    feat(API): Método GET ONE funcionando

--- backend/src/main/java/com/kingspan/backend/controllers/SystemController.java
+++ backend/src/main/java/com/kingspan/backend/controllers/SystemController.java
@@ -1,19 +1,25 @@
 package com.kingspan.backend.controllers;
 
 import java.util.List;
+import java.util.UUID;
+import java.util.Optional;
+
+import jakarta.validation.Valid;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.kingspan.backend.dtos.SystemRecordDto;
 import com.kingspan.backend.models.SystemModel;
 import com.kingspan.backend.repositories.SystemRepository;
-import jakarta.validation.Valid;
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class SystemController {
@@ -32,4 +38,14 @@ public class SystemController {
     public ResponseEntity<List<SystemModel>> getAllSystems(){
         return ResponseEntity.status(HttpStatus.OK).body(systemRepository.findAll());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOneSystem(@PathVariable(value="id") UUID id){
+        Optional<SystemModel> systemOptional = systemRepository.findById(id);
+        if(systemOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Sistema não encontrado!");
+        } else{
+            return ResponseEntity.status(HttpStatus.OK).body(systemOptional.get());
+        }
+    } 
 }
