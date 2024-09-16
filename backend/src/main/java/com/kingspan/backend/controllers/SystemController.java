8dd727e818c7082d74980b8c093eb9f420ee6a94
saraklamann <saracarolineklamannjucoski@gmail.com>
2024-09-15 23:24:07 -0300

    docs(swagger): Configuração e geração da documentação da API utilizando swagger

--- backend/src/main/java/com/kingspan/backend/controllers/SystemController.java
+++ backend/src/main/java/com/kingspan/backend/controllers/SystemController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kingspan.backend.dtos.SystemRecordDto;
@@ -23,7 +24,13 @@ import com.kingspan.backend.models.SystemModel;
 import com.kingspan.backend.repositories.SystemRepository;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 @RestController
+@RequestMapping("/")
 @CrossOrigin(origins = "http://localhost:3000")
 public class SystemController {
 
@@ -31,6 +38,10 @@ public class SystemController {
     SystemRepository systemRepository;
 
     @PostMapping(value = "")
+    @Operation(summary = "Cria novo sistema")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Operação realizada com sucesso!")
+    })
     public ResponseEntity <SystemModel> saveSystem(@RequestBody @Valid SystemRecordDto systemRecordDto){
         var systemModel = new SystemModel();
         BeanUtils.copyProperties(systemRecordDto, systemModel);
@@ -38,22 +49,36 @@ public class SystemController {
     }
 
     @GetMapping("")
+    @Operation(summary = "Lista todos os sistemas")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Operação realizada com sucesso!")
+    })
     public ResponseEntity<List<SystemModel>> getAllSystems(){
         return ResponseEntity.status(HttpStatus.OK).body(systemRepository.findAll());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Object> getOneSystem(@PathVariable(value="id") UUID id){
+    @Operation(summary = "Retorna um sistema com base no UUID (ID)")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "404", description = "Nenhum Sistema foi encontrado. Favor revisar os critérios da sua pesquisa!"),
+        @ApiResponse(responseCode = "200", description = "Operação realizada com sucesso!")
+    })
+    public ResponseEntity<Object> getOneSystem(@Parameter(description = "UUID (ID) do sistema") @PathVariable(value="id") UUID id){
         Optional<SystemModel> systemOptional = systemRepository.findById(id);
         if(systemOptional.isEmpty()){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Sistema não encontrado!");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Nenhum Sistema foi encontrado. Favor revisar os critérios da sua pesquisa!");
         } else{
             return ResponseEntity.status(HttpStatus.OK).body(systemOptional.get());
         }
     } 
 
     @PutMapping("/{id}")
-    public ResponseEntity<Object> updateSystem(@PathVariable(value="id") UUID id, @RequestBody @Valid SystemRecordDto systemRecordDto){
+    @Operation(summary = "Altera um sistema com base no UUID (ID)")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "404", description = "Nenhum Sistema foi encontrado. Favor revisar os critérios da sua pesquisa!"),
+        @ApiResponse(responseCode = "200", description = "Operação realizada com sucesso!")
+    })
+    public ResponseEntity<Object> updateSystem(@Parameter(description = "UUID (ID) do sistema") @PathVariable(value="id") UUID id, @RequestBody @Valid SystemRecordDto systemRecordDto){
         Optional<SystemModel> systemOptional = systemRepository.findById(id);
         if(systemOptional.isEmpty()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Sistema não encontrado!");
@@ -65,13 +90,18 @@ public class SystemController {
     }
     
     @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deleteSystem(@PathVariable(value="id") UUID id){
+    @Operation(summary = "Deleta um sistema com base no UUID (ID)")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "404", description = "Nenhum Sistema foi encontrado. Favor revisar os critérios da sua pesquisa!"),
+        @ApiResponse(responseCode = "200", description = "Operação realizada com sucesso!")
+    })
+    public ResponseEntity<Object> deleteSystem(@Parameter(description = "UUID (ID) do sistema") @PathVariable(value="id") UUID id){
         Optional<SystemModel> systemOptional = systemRepository.findById(id);
         if(systemOptional.isEmpty()){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Sistema não encontrado!");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Nenhum Sistema foi encontrado. Favor revisar os critérios da sua pesquisa!");
         } else {
             systemRepository.delete(systemOptional.get());
-            return ResponseEntity.status(HttpStatus.OK).body("Sistema deletado com sucesso!");
+            return ResponseEntity.status(HttpStatus.OK).body("Operação realizada com sucesso!");
         } 
     }
 }
