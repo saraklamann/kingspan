d12d3ab8d0b3af674bf40d9c4adbf9796cdeef58
saraklamann <saracarolineklamannjucoski@gmail.com>
2024-09-16 23:04:39 -0300

    style(editar sistema): Estilização completa da página 'Editar sistema'

--- frontend/src/components/edit-system-form.js
+++ frontend/src/components/edit-system-form.js
@@ -2,45 +2,62 @@ import "../styles/edit-system/system-form-component/style.css";
 
 const EditSystem = ({ system, handleChange, handleSubmit, reason}) => {
     return (
-        <div>
+        <div className="justify-center">
             <form onSubmit={handleSubmit}>
-                <div>
-                    <label> Descrição: </label>
-                    <input type="text" name="systemDescription" value={system.systemDescription} onChange={handleChange} required />
+                <div className="row">
+                    <div className="col-lg-4 col-md-4 col-sm-4">
+                        <label> Descrição: </label>
+                        <input className="input-field" type="text" name="systemDescription" value={system.systemDescription} onChange={handleChange} required />
+                    </div>
+                    <div className="col-lg-4 col-md-4 col-sm-4">
+                        <label> Sigla:</label>
+                        <input className="input-field" type="text" name="systemAcronym" value={system.systemAcronym} onChange={handleChange} required />
+                    </div>
+
+                    <div className="col-lg-4 col-md-4 col-sm-4">
+                        <label> Email de atendimento:</label>
+                        <input className="input-field" type="text" name="systemServiceEmail" value={system.systemServiceEmail} onChange={handleChange} />
+                    </div>
                 </div>
-                <div>
-                    <label> Sigla:</label>
-                    <input type="text" name="systemAcronym" value={system.systemAcronym} onChange={handleChange} required />
+
+                <div className="row">
+                    <div className="col-lg-4 col-md-4 col-sm-4">
+                        <label> Url:</label>
+                        <input className="input-field" type="text" name="systemUrl" value={system.systemUrl} onChange={handleChange} />
+                    </div>
+                
+                    <div className="col-lg-4 col-md-4 col-sm-4">
+                        <label> Status:</label>
+                        <input className="input-field" type="text" name="systemStatus" value={system.systemStatus} onChange={handleChange}  required/>
+                    </div>
+
+                    <div className="col-lg-4 col-md-4 col-sm-4">
+                        <label> Usuário responsável pela última alteração:</label>
+                        <input className="input-field" type="text" name="systemLastEditor" value={system.systemLastEditor} readOnly />
+                    </div>
                 </div>
-                <div>
-                    <label> Email de atendimento:</label>
-                    <input type="text" name="systemServiceEmail" value={system.systemServiceEmail} onChange={handleChange} />
+
+                <div className="row">
+                    <div className="col-lg-4 col-md-4 col-sm-4">
+                        <label> Data da última alteração:</label>
+                        <input className="input-field" type="text" name="systemLastUpdate" value={system.systemLastUpdate} readOnly/>
+                    </div>
+
+                    <div className="col-lg-4 col-md-4 col-sm-4">
+                        <label> Última justificativa de edição:</label>
+                        <input className="input-field" type="text" name="lastUpdate" value={system.systemUpdateJustification} readOnly/>
+                    </div>
+
+                    <div className="col-lg-4 col-md-4 col-sm-4">
+                        <label> Motivo da alteração:</label>
+                        <input className="input-field" type="text" name="systemUpdateJustification" value={reason.oldJustification} onChange={handleChange} required/>
+                    </div>
                 </div>
-                <div>
-                    <label> Url:</label>
-                    <input type="text" name="systemUrl" value={system.systemUrl} onChange={handleChange} />
+                <div className="row">
+                    <div className="col-lg-12 col-md-12 col-sm-12 justify-center">
+                        <button className="all-button submit-button" type="submit">Salvar</button>
+                    </div>
                 </div>
-                <div>
-                    <label> Status:</label>
-                    <input type="text" name="systemStatus" value={system.systemStatus} onChange={handleChange}  required/>
-                </div>
-                <div>
-                    <label> Usuário responsável pela última alteração:</label>
-                    <input type="text" name="systemLastEditor" value={system.systemLastEditor} readOnly />
-                </div>
-                <div>
-                    <label> Data da última alteração:</label>
-                    <input type="text" name="systemLastUpdate" value={system.systemLastUpdate} readOnly/>
-                </div>
-                <div>
-                    <label> Última justificativa de edição:</label>
-                    <input type="text" name="lastUpdate" value={system.systemUpdateJustification} readOnly/>
-                </div>
-                <div>
-                    <label> Motivo da alteração:</label>
-                    <input type="text" name="systemUpdateJustification" value={reason.oldJustification} onChange={handleChange} required/>
-                </div>
-                <button type="submit">Salvar</button>
             </form>
         </div>
     );
--- frontend/src/pages/edit-system-page.js
+++ frontend/src/pages/edit-system-page.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import SystemForm from '../components/edit-system-form';
+import { Link } from 'react-router-dom';
+import "../styles/edit-system/edit-system-page/style.css";
+import arrowBack from '../assets/icons/arrow-back.svg'; // Caminho relativo à imagem
 
 const EditSystemPage = () => {
     const { id } = useParams();
@@ -56,10 +59,31 @@ const EditSystemPage = () => {
     if (error) return <p>Error: {error.message}</p>;
 
     return (
-        <div>
-            <h1> Editar sistema </h1>
-            {error && <div className="error">{error.message}</div>}
-            <SystemForm system={system} handleChange={handleChange} handleSubmit={handleSubmit} reason={oldJustification}/>
+        <div className='main-element'>
+            <div className='row'>
+                <div className='col-lg-12 col-md-12 col-sm-12 margin-bottom'>
+                    <Link to="/" className='previous-page-button'>
+                        <img src={arrowBack} alt="Seta para voltar para página anterior"/>
+                        <span> Voltar para página inicial </span>
+                    </Link>
+                </div>
+            </div>
+            <div className='justify-center'>
+                <div className='frame'>
+                    <div className='row'>
+                        <div className='col-lg-12 col-md-12 col-sm-12'>
+                            <h1 className='title'> Dados e controle do sistema </h1>
+                        </div>
+                    </div>
+
+                    <div className='row'>
+                        <div className='col-lg-12 col-md-12 col-sm-12'>
+                            {error && <div className="error ">{error.message}</div>}
+                            <SystemForm system={system} handleChange={handleChange} handleSubmit={handleSubmit} reason={oldJustification}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
